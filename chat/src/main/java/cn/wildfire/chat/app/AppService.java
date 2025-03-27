3c08e648bb7601902202bd526f3f32bb28c434c5
zihe2018 <45512044+zihe2018@users.noreply.github.com>
2025-03-27 22:49:51 +0800

    Update AppService.java

--- chat/src/main/java/cn/wildfire/chat/app/AppService.java
+++ chat/src/main/java/cn/wildfire/chat/app/AppService.java
@@ -55,7 +55,7 @@ public class AppService implements AppServiceProvider {
      * <br>
      */
 //    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释，http 前缀不能省略*/ = "http://wildfirechat.net:8888";
-    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释*/ = "https://app.wildfirechat.net";
+    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释*/ = "http://192.168.6.2:8888";
 
     private AppService() {
 
